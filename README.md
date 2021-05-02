d1446496905f68119846d599081766eafcfb54ed
Shivam Oswal <shivam.oswal@gmail.com>
2021-05-03 02:19:32 +0530

    python scheduler to get slots for vaccination

---
+++ README.md
@@ -0,0 +1,13 @@
+# Pre-requisites:
+
+Python3
+
+
+# Setup:
+
+You will have to grant access to this script for sending emails from your inbox using this steps mentioned here: https://hotter.io/docs/email-accounts/secure-app-gmail/
+
+After this is done, just edit the config part of script to set the pincode you want to search, age group and your email and password  and it will automatically serach slots for that date.
+
+The job will run everyday at 12:00 by default. You can modify that if you wish to.
+
---
+++ get_slots_for_vaccination.py
@@ -0,0 +1,67 @@
+import requests
+import smtplib
+import datetime
+import schedule
+import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+#configs
+min_age_limit = 45
+pincode = "413312"
+email = "your email"
+password = "your pass"
+
+def get_current_date():
+    date = datetime.datetime.now()
+    date = strftime("%d-%m-%Y")
+    return date
+
+def get_api_response():
+    url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin"
+    date = get_current_date()
+    params = {"pincode":413132, "date": date}
+    headers = {"accept": "application/json", "Accept-Language": "hi_IN"}
+    response = requests.get(url=url, headers=headers, params=params)
+    res = response.json()
+    contents = "Available slots::\n"
+    for session in res['sessions']:
+        if session["min_age_limit"] == min_age_limit and session["slots"] != []:
+            contents = contents + "Date::" + " " + str(session['date']) + "\n"
+            contents = contents + "Slots::" + " " + str(session['slots']) + "\n"
+            contents = contents + "Center_id::" + " " + str(session['center_id']) + "\n"
+            contents = contents + "Name::" + " " + str(session['name']) + "\n"
+            contents = contents + "\n\n"
+
+    return contents
+
+def get_current_date():
+    date = datetime.datetime.now()
+    date = date.strftime("%d-%m-%Y")
+    return date
+
+def send_email(contents):
+    mail_content = contents
+    sender_address = email
+    sender_pass = password
+    receiver_address = email
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'Vaccination alert: Slots are available for booking!'   #The subject line
+    message.attach(MIMEText(mail_content, 'plain'))
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(sender_address, sender_pass) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('Mail Sent')
+
+def run_core_functionality(time):
+    print("Running the job at: "+(time))
+    contents = get_api_response()
+    send_email(contents)
+
+schedule.every().day.at("12:00").do(run_core_functionality,'It is 12:00')
